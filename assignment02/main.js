5de1b968b7d3782617f264d8e48a67ce5573a1a3
evoucou <evoucou@users.noreply.github.com>
2018-10-07 15:19:57 -0400

    Added the eyes

--- assignment02/main.js
+++ assignment02/main.js
@@ -1,9 +1,8 @@
 
+window.onload = function() {
 
 var images = [];
 
-// Note to myself: we first draw parts that are the farthest away
-// Order is: arms, head, legs, torso, eyes...
 loadImage("leftarm");
 loadImage("leftleg");
 loadImage("torso");
@@ -23,35 +22,60 @@ function loadImage(name) {
 var totalResources = 6;
 var numResourcesLoaded = 0;
 var fps = 30;
-var charX = 45;
-var charY = 85;
+
+// character X, where the central element is the torso.
+// Basically, charX = torsoX.
 
 function resourceLoaded() {
 
 // checking if all images have been laoded, if so we trigger 'draw' function
 numResourcesLoaded += 1;
 if(numResourcesLoaded === totalResources) {
-  setInterval(redraw(charX, charY), 1000 / fps);
+  setInterval(redraw, 1000 / fps);
 }
-console.log(numResourcesLoaded);
 }
 
-// character X, where the central element is the torso.
-// Basically, charX = torsoX.
+function redraw() {
 
-function redraw(x,y) {
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
 
-this.x = charX;
-this.y = charY;
+var x = canvas.width/2;
+var y = 100;
 
-var context = document.getElementById("myCanvas").getContext("2d");
+  //context.width = context.width; // clears the canvas
 
-  context.width = context.width; // clears the canvas
+  // Note to myself: we first draw parts that are the farthest away
+  // Order is: arms, head, legs, torso, eyes...
+context.drawImage(images["leftarm"], x-30,y);
+context.drawImage(images["rightarm"], x+30,y);
+context.drawImage(images["head"], x, y);
+context.drawImage(images["leftleg"], x, y + 50);
+context.drawImage(images["rightleg"], x - 15, y - 42);
+context.drawImage(images["torso"], x, y - 50);
 
-context.drawImage(images["leftarm"], this.x + 40, this.y - 42);
-context.drawImage(images["leftleg"], this.x, this.y);
-context.drawImage(images["torso"], this.x, this.y - 50);
-context.drawImage(images["rightarm"], this.x - 15, y - 42);
-context.drawImage(images["rightleg"], this.x - 15, this.y - 42);
-context.drawImage(images["head"], this.x - 10, this.y - 125);
+drawEllipse(x, y, 19, 16); // Left Eye
+drawEllipse(x, y, 19, 16); // Right Eye
+
+function drawEllipse(centerX, centerY, width, height) {
+
+  context.beginPath();
+  context.moveTo(centerX, centerY - height/2);
+
+  context.bezierCurveTo(
+    centerX + width/2, centerY - height/2,
+    centerX + width/2, centerY + height/2,
+    centerX, centerY + height/2);
+
+  context.bezierCurveTo(
+    centerX - width/2, centerY + height/2,
+    centerX - width/2, centerY - height/2,
+    centerX, centerY - height/2);
+
+  context.fillStyle = "black";
+  context.fill();
+  context.closePath();
+}
+
+}
 }
