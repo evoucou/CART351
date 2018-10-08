ed84107acf3600ba04cc51da18356fee442bf9f3
evoucou <evoucou@users.noreply.github.com>
2018-10-08 12:58:49 -0400

    Loading image with HTML code
    And animation in JS

--- assignment02/main.js
+++ assignment02/main.js
@@ -1,129 +1,30 @@
-window.onload = function() {
 
-var images = [];
 
-loadImage("leftarm");
-loadImage("leftleg");
-loadImage("torso");
-loadImage("rightarm");
-loadImage("rightleg");
-loadImage("head");
+var rightLeg = null;
+var leftLeg = null;
+var rightArm = null;
+var leftArm = null;
+var head = null;
+var torso = null;
 
-var legImg = new Image();
-legImg.src = "sources/png/rightleg.png";
+var animate;
 
-function loadImage(name) {
+var fps = 30;
 
-  images[name] = new Image();
-  images[name].onload = function() {
-      resourceLoaded();
-  }
-  images[name].src = "sources/png/" + name + ".png";
+function init(){
+   torso = document.getElementById('torsoImg');
+   torso.style.position= 'relative';
+   torso.style.left = '0px';
 }
 
-var totalResources = 6;
-var numResourcesLoaded = 0;
-var fps = 10;
-
-function resourceLoaded() {
-
-// checking if all images have been laoded, if so we trigger 'draw' function
-numResourcesLoaded += 1;
-if(numResourcesLoaded === totalResources) {
-  setInterval(redraw, 1000 / fps);
-}
+function moveRight(){
+   torso.style.left = parseInt(torso.style.left) + 10 + 'px';
+   animate = setTimeout(moveRight,1000 / fps); // call moveRight in 20msec
 }
 
-function redraw() {
-
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
-
-  //context.width = context.width; // clears the canvas
-
-var torsoWidth = images["torso"].width;
-var torsoHeight = images["torso"].height;
-
-var armWidth = images["rightarm"].width;
-var armHeight = images["rightarm"].height;
-
-var legWidth = images["rightleg"].width;
-var legHeight = images["rightleg"].height;
-
-var headWidth = images["head"].width;
-var headHeight = images["head"].height;
-
-//The center of the canvas is the torso.
-var x = canvas.width/2;
-var y = canvas.height/2;
-
-  // Note to myself: we first draw parts that are the farthest away
-  // Order is: arms, head, legs, torso, eyes...
-context.drawImage(images["leftarm"], x - armWidth/2, y + 30);
-context.drawImage(images["rightarm"], x + (torsoWidth - armWidth/2), y + 30);
-context.drawImage(images["head"], x - (headWidth/8 + 4), y - (torsoHeight + 15));
-context.drawImage(images["leftleg"], x - (torsoWidth/2 - legWidth/2 + 5), y + legHeight/2);
-context.drawImage(images["rightleg"], x + (torsoWidth - legWidth + 23), y + legHeight/2);
-context.drawImage(images["torso"], x, y);
-
-////anim/////
-var angle = 0;
-
-// add an event listener
-canvas.addEventListener('mousedown', (event) => {
-  requestAnimationFrame(rotateArm);
-});
-        function incrementAngle() {
-               angle++;
-               if(angle > 360) {
-                   angle = 0;
-               }
-           }
-
-
-function rotateArm() {
-
-  incrementAngle();
-  context.drawImage(legImg, x+10, y+10);
-
-   context.translate(x,y);
-   context.rotate(-angle + Math.PI/2.0);
-  context.drawImage(legImg, x+10, y+10);
-   context.rotate(angle - Math.PI/2.0);
-   context.translate(-x, -y);
-
-  requestAnimationFrame(rotateArm);
-  console.log(angle);
-
-
+function stop(){
+   clearTimeout(animate);
+   torso.style.left = '0px';
 }
 
-
-/////////
-
-// We want to make him blink, so it's easier to draw shapes
-drawEllipse(x + 33, y - 85, 19, 15); // Left Eye
-drawEllipse(x + 79, y - 85, 19, 15); // Right Eye
-
-function drawEllipse(centerX, centerY, width, height) {
-
-  context.beginPath();
-  context.moveTo(centerX, centerY - height/2);
-
-  context.bezierCurveTo(
-    centerX + width/2, centerY - height/2,
-    centerX + width/2, centerY + height/2,
-    centerX, centerY + height/2);
-
-  context.bezierCurveTo(
-    centerX - width/2, centerY + height/2,
-    centerX - width/2, centerY - height/2,
-    centerX, centerY - height/2);
-
-  context.fillStyle = "black";
-  context.fill();
-  context.closePath();
-}
-
-}
-}
+window.onload = init;
