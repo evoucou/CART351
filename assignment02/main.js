04140ae9d89ed920eac358966e8992d1f761c510
evoucou <evoucou@users.noreply.github.com>
2018-10-08 18:50:23 -0400

    Images are loaded

--- assignment02/main.js
+++ assignment02/main.js
@@ -1,57 +1,37 @@
-window.onload = function() {
+window.onload = init;
 
 var character = new Image();
 var rightHit = new Image();
 var leftHit = new Image();
 
 character.src = "sources/character.gif";
-rightHit.src="sources/leftside.png";
-leftHit.src="sources/rightside.png";
+rightHit.src="sources/leftside.gif";
+leftHit.src="sources/rightside.gif";
 
 var fps = 10;
 
-function resourceLoaded() {
-  setInterval(redraw, 1000 / fps);
+function init() {
+  setInterval(draw, 1000 / fps);
 }
 
-function redraw() {
+function draw() {
 
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
-var x = canvas.width/2;
-var y = canvas.height/2;
+canvas.addEventListener('mousedown', (event) => {
+   console.log('canvas mousedown');
+
+
+});
+
+var x = 100;
+var y = 100;
 
 context.drawImage(character,x,y)
-context.drawImage(leftHit,x,y)
-context.drawImage(rightHits,x,y)
-
-
-
-/////////
-
-// We want to make him blink, so it's easier to draw shapes
-drawEllipse(x + 33, y - 85, 19, 15); // Left Eye
-drawEllipse(x + 79, y - 85, 19, 15); // Right Eye
-
-function drawEllipse(centerX, centerY, width, height) {
-
-  context.beginPath();
-  context.moveTo(centerX, centerY - height/2);
-
-  context.bezierCurveTo(
-    centerX + width/2, centerY - height/2,
-    centerX + width/2, centerY + height/2,
-    centerX, centerY + height/2);
-
-  context.bezierCurveTo(
-    centerX - width/2, centerY + height/2,
-    centerX - width/2, centerY - height/2,
-    centerX, centerY - height/2);
-
-  context.fillStyle = "black";
-  context.fill();
-  context.closePath();
-}
+//context.drawImage(leftHit,x,y)
+//context.drawImage(rightHit,x,y)
 
 }
+
+window.onload = init;
