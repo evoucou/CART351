92ff747545ce8d5cc86008685c0029d66ad41e40
evoucou <evoucou@users.noreply.github.com>
2018-10-08 14:36:59 -0400

    Going to use .GIF

--- assignment02/main.js
+++ assignment02/main.js
@@ -1,30 +1,5 @@
+window.onload = function() {
 
 
-var rightLeg = null;
-var leftLeg = null;
-var rightArm = null;
-var leftArm = null;
-var head = null;
-var torso = null;
 
-var animate;
-
-var fps = 30;
-
-function init(){
-   torso = document.getElementById('torsoImg');
-   torso.style.position= 'relative';
-   torso.style.left = '0px';
 }
-
-function moveRight(){
-   torso.style.left = parseInt(torso.style.left) + 10 + 'px';
-   animate = setTimeout(moveRight,1000 / fps); // call moveRight in 20msec
-}
-
-function stop(){
-   clearTimeout(animate);
-   torso.style.left = '0px';
-}
-
-window.onload = init;
