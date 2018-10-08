b3ce4ee337f54e28d7c14b4e4ad50055ab685e8a
evoucou <evoucou@users.noreply.github.com>
2018-10-08 19:42:12 -0400

    added eventlistener

--- assignment02/main.js
+++ assignment02/main.js
@@ -1,37 +1,27 @@
+
+
 window.onload = init;
-
-var character = new Image();
-var rightHit = new Image();
-var leftHit = new Image();
-
-character.src = "sources/character.gif";
-rightHit.src="sources/leftside.gif";
-leftHit.src="sources/rightside.gif";
-
-var fps = 10;
-
+  //var fps = 10;
+//setInterval(init, 1000 / fps);
 function init() {
-  setInterval(draw, 1000 / fps);
+  var character = new Image();
+  character.src = "sources/character.gif";
+  var x = 100;
+  var y = 100;
+  var canvas = document.getElementById("myCanvas");
+  var context = canvas.getContext("2d");
+  canvas.addEventListener("click", function(){ draw(x,y,context); });
 }
 
-function draw() {
+function draw(x,y,context) {
+  var rightHit = new Image();
+  var leftHit = new Image();
+  rightHit.src="sources/leftside.gif";
+  leftHit.src="sources/rightside.gif";
+  console.log('canvas mousedown');
+  context.drawImage(leftHit,x,y);
+}
 
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
-
-canvas.addEventListener('mousedown', (event) => {
-   console.log('canvas mousedown');
-
-
-});
-
-var x = 100;
-var y = 100;
-
-context.drawImage(character,x,y)
+//context.drawImage(character,x,y)
 //context.drawImage(leftHit,x,y)
 //context.drawImage(rightHit,x,y)
-
-}
-
-window.onload = init;
