626aac5463e613bb12232282b86f5b82a938b67d
evoucou <evoucou@users.noreply.github.com>
2018-10-08 20:09:01 -0400

    Going with frames rather than .gif

--- assignment02/main.js
+++ assignment02/main.js
@@ -5,19 +5,19 @@ window.onload = init;
 //setInterval(init, 1000 / fps);
 function init() {
   var character = new Image();
+  var rightHit = new Image();
+  var leftHit = new Image();
   character.src = "sources/character.gif";
+  rightHit.src="sources/leftside.gif";
+  leftHit.src="sources/rightside.gif";
   var x = 100;
   var y = 100;
   var canvas = document.getElementById("myCanvas");
   var context = canvas.getContext("2d");
-  canvas.addEventListener("click", function(){ draw(x,y,context); });
+  canvas.addEventListener("click", function(){ draw(x,y,context,rightHit,leftHit );});
 }
 
-function draw(x,y,context) {
-  var rightHit = new Image();
-  var leftHit = new Image();
-  rightHit.src="sources/leftside.gif";
-  leftHit.src="sources/rightside.gif";
+function draw(x,y,context,rightHit,leftHit ) {
   console.log('canvas mousedown');
   context.drawImage(leftHit,x,y);
 }
