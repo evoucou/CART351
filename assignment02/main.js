bf2f76815df4b3285391cd25f484c43d2461ab5d
evoucou <evoucou@users.noreply.github.com>
2018-10-09 10:54:56 -0400

    Modifying code in order to have one code for both sides animations

--- assignment02/main.js
+++ assignment02/main.js
@@ -4,56 +4,66 @@ window.onload = init;
   //var fps = 10;
 //setInterval(init, 1000 / fps);
 function init() {
-  var character = new Image();
-  var rigthHitAnim = [];
-  var leftHitAnim = [];
-  character.src = "sources/character.gif";
-  var name = "righthit";
-  rigthHitAnim = loadAnim(name);
-  name = "lefthit";
-  leftHitAnim = loadAnim(name);
-  var x = 100;
-  var y = 100;
+
+  //var rigthHitAnim = [];
+  //var leftHitAnim = [];
+  //var name;
+  //name = "lefthit";
+  //leftHitAnim = loadAnim(name);
+
+  //console.log(leftHit);
+  var name;
+  var x = 0;
+  var y = 0;
   var canvas = document.getElementById("myCanvas");
   var context = canvas.getContext("2d");
-  canvas.addEventListener("click", function(){draw(x,y,canvas,context,leftHitAnim);});
+
+  var character = new Image();
+  character.src = "sources/character.gif";
+  context.drawImage(character,x,y);
+
+  canvas.addEventListener("mousedown", function(){loadAnim("righthit",x,y,canvas,context);});
+  canvas.addEventListener("keydown", function(){loadAnim("lefthit",x,y,canvas,context);});
 }
 
-async function draw(x,y,canvas,context,leftHitAnim) {
-  var timer =10000;
-  var i=0;
-  while(i<6)
-   {
-    if(i==0) {
+
+
+async function loadAnim(name,x,y,canvas,context) {
+  //console.log("called");
+  var anim =[];
+  for(var i=1;i<7;i++){
+    anim[i]=new Image();
+    anim[i].src= "sources/"+name+"/frame"+i+".gif";
+
+    if(i==1) {
       //Display initial image
-      context.drawImage(leftHitAnim[i],x,y);
+      context.drawImage(anim[i],x,y);
     } else {
           //hiddeImage();
-          showImage(leftHitAnim[i],context,canvas,x,y);
+          context.clearRect(0,0,canvas.width,canvas.height)
+          context.drawImage(anim[i],x,y);
+          console.log(i);
     }
     await sleep(50);
     i++;
-
+    //The animation disappears once fully loaded
+    context.clearRect(0,0,canvas.width,canvas.height)
   }
-  //clearTimeout(timer);
-}
-function showImage(leftHitAnim,context,canvas,x,y) {
-  context.clearRect(0,0,canvas.width,canvas.height)
-  context.drawImage(leftHitAnim,x,y);
-}
 
-function loadAnim(name) {
-  var anim =[];
-  for(var i=0;i<6;i++){
-    anim[i]=new Image();
-    anim[i].src= "sources/"+name+"/frame"+i+".gif";
+  function showImage(anim,context,canvas,x,y) {
+    //Clears previous frame every time
+    context.clearRect(0,0,canvas.width,canvas.height)
+    context.drawImage(anim,x,y);
   }
-return anim;
+
+   function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+
 }
 
- function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 
 
